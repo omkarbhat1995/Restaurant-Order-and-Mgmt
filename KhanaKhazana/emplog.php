2513a72010fe1bf6174a8dbd045a4b307ca4f055
omkarbhat1995 <omkarbhat1995@gmail.com>
2019-04-19 22:54:59 -0400

    Create emplog.php

---
+++ KhanaKhazana/emplog.php
@@ -0,0 +1,164 @@
+<?php 
+
+    // First we execute our common code to connection to the database and start the session 
+    require("common.php"); 
+     
+    // This variable will be used to re-display the user's username to them in the 
+    // login form if they fail to enter the correct password.  It is initialized here 
+    // to an empty value, which will be shown if the user has not submitted the form. 
+    $submitted_username = ''; 
+     
+    // This if statement checks to determine whether the login form has been submitted 
+    // If it has, then the login code is run, otherwise the form is displayed 
+    if(!empty($_POST)) 
+    { 
+        // This query retreives the user's information from the database using 
+        // their username. 
+        $query = " SELECT Email_id,Emp_id,Password FROM Employee WHERE Email_id = :username"; 
+         
+        // The parameter values 
+        $query_params = array( 
+            ':username' => $_POST['username'] 
+        ); 
+         
+        try 
+        { 
+            // Execute the query against the database 
+            $stmt = $db->prepare($query); 
+            $result = $stmt->execute($query_params); 
+        } 
+        catch(PDOException $ex) 
+        { 
+            // Note: On a production website, you should not output $ex->getMessage(). 
+            // It may provide an attacker with helpful information about your code.  
+            die("Failed to run query: " . $ex->getMessage()); 
+        } 
+         
+        // This variable tells us whether the user has successfully logged in or not. 
+        // We initialize it to false, assuming they have not. 
+        // If we determine that they have entered the right details, then we switch it to true. 
+        $login_ok = false; 
+         
+        // Retrieve the user data from the database.  If $row is false, then the username 
+        // they entered is not registered. 
+        $row = $stmt->fetch(); 
+        if($row) 
+        { 
+            // Using the password submitted by the user and the salt stored in the database, 
+            // we now check to see whether the passwords match by hashing the submitted password 
+            // and comparing it to the hashed version already stored in the database. 
+            $check_password = hash('sha256', $_POST['password']);
+			$id=$row['Emp_id'];
+			//echo($check_password);echo("\n");echo("\n");echo("\n");echo("\n");
+			//echo($row);
+            if($check_password === $row['Password']) 
+            { 	
+                // If they do, then we flip this to true 
+                $login_ok = true; 
+            } 
+        } 
+			define ('DB_Name','og7ut_khana_khazana');
+			define ('DB_User','og7ut');
+			define ('DB_Pass','bXnQZS1k');
+			define ('DB_Host','mysql.cs.virginia.edu');
+			$link= mysqli_connect(DB_Host,DB_User,DB_Pass,DB_Name);
+
+		$u=$_POST['username'];
+		//echo($u);
+//        $sql3="INSERT INTO loggedin (username) VALUES ('$u')";
+  //      if (!mysqli_query($link,$sql3)){die('Error'.mysqli_error($link));}
+
+		 
+        // If the user logged in successfully, then we send them to the private members-only page 
+        // Otherwise, we display a login failed message and show the login form again 
+        if($login_ok) 
+        { 
+            // Here I am preparing to store the $row array into the $_SESSION by 
+            // removing the salt and password values from it.  Although $_SESSION is 
+            // stored on the server-side, there is no reason to store sensitive values 
+            // in it unless you have to.  Thus, it is best practice to remove these 
+            // sensitive values first. 
+            
+            unset($row['Password']); 
+             
+            // This stores the user's data into the session at the index 'user'. 
+            // We will check this index on the private members-only page to determine whether 
+            // or not the user is logged in.  We can also use it to retrieve 
+            // the user's details. 
+			session_start();
+            $_SESSION['user'] = $row; 
+			//localStorage.setItem('user','$u');//["user"]=$u;
+			$url="empinfo.php?username=".$u."&id=".$id;
+            header("Location: ".$url); 
+            //Redirect the user to the private members-only page. 
+            ####################################################################die("Redirecting to: private.php"); 
+        } 
+        else 
+        { 
+            // Tell the user they failed 
+            print("Login Failed."); 
+           // Show them their username again so all they have to do is enter a new 
+            // password.  The use of htmlentities prevents XSS attacks.  You should 
+            // always use htmlentities on user submitted values before displaying them 
+            // to any users (including the user that submitted them).  For more information: 
+            // http://en.wikipedia.org/wiki/XSS_attack 
+            $submitted_username = htmlentities($_POST['username'], ENT_QUOTES, 'UTF-8'); 
+        } 
+    } 
+     
+?> 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" width="device-width, initial-scale=1.0">
+	<title>The Indian Cuisine - Contact</title>
+	<link rel="icon" href="img/favicon.png" type="image/x-icon"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/contact.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="col-md-12 contact">
+			<nav>
+				<div class="row">
+					<div class="col-md-3 brand">
+						<img src="img/logo.png" alt="img" class="img-fluid">
+					</div>
+					<div class="col-md-9 list">
+						<ul>
+							<li><a href="index.php">HOME</a></li>
+							<li><a href="menu.php">MENU</a></li>
+							<li><a href="gallery.php">GALLERY</a></li><li><a href="login.php">ORDER</a></li>
+							<li><a href="contact.php">CONTACT</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		 	<div class="main-part" align="center">
+		 		<h3 class="text-center">Employee Login</h3>
+			 	<hr>
+						<form action="emplog.php" method="post" align="center"> 
+							Username:<br /> 
+								<input type="text" name="username" />        
+							<br /><br /> 
+							Password:<br /> 
+								<input type="password" name="password"/> 
+							<br /><br /> 
+								<input type="submit" value="Login" /> 
+						
+					<br><br><br><br><br><br><br><br>
+<a href="register.php">Register</a><br><br><br><br><br><br><br><br>
+						</form> 
+						
+					</div>
+
+		 	</div>
+		</div>
+	</div>
+</body>
+</html>
